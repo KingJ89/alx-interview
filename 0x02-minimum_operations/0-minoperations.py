4648abf97985243100e79cf8b00c244c7ef531c5
KingJ89 <janmutewera@gmail.com>
2024-10-21 02:23:29 +0000

    Task Min ops completed successfully

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+"""
+Module that calculates the fewest number of operatons
+needed to result in exactly n 'H' characters in the file.
+"""
+def minOperations(n):
+    """
+    Determines the minimum number of operations needed to achieve exactly
+    n 'H' characters in a file using only 'Copy All' and 'Paste' operations.
+    
+    Args:
+    n (int): The target number of 'H' characters.
+        
+        Returns:
+    int: The minimum number of opertions required. If n is les than or equal to 1, returns 0.
+        """
+        
+        if n < 2:
+            return 0
+        
+        operations = 0
+        divisor = 2
+        
+        # Factorize n, summing up the factors as the operation count
+        while n > 1:
+            while n % divisor == 0:
+                operations += divisor
+                n //= divisor
+                
+                divisor += 1
+                
+                return operations
---
+++ 0x02-minimum_operations/README.md
@@ -0,0 +1,1 @@
+this is a non_empty readme file on the ALX project minimum operations by Jan Mutewera
